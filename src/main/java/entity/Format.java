28b14f12a9f4c39fd8aaf76ed6aefb20b6c0ffe6
Tamino1762 <paganinifreak@gmail.com>
2020-04-08 13:01:04 -0500

    Added classes for publisher and format.

---
+++ src/main/java/entity/Format.java
@@ -0,0 +1,26 @@
+package entity;
+
+public class Format {
+
+    private int formatId;
+    private String type;
+
+    public Format() {
+    }
+
+    public int getFormatId() {
+        return formatId;
+    }
+
+    public void setFormatId(int formatId) {
+        this.formatId = formatId;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
---
+++ src/main/java/entity/Publisher.java
@@ -0,0 +1,53 @@
+package entity;
+
+public class Publisher {
+
+    private int publisherId;
+    private String publisher;
+    private String publisher2;
+    private String publisher3;
+    private String publisher4;
+
+    public Publisher() {
+    }
+
+    public int getPublisherId() {
+        return publisherId;
+    }
+
+    public void setPublisherId(int publisherId) {
+        this.publisherId = publisherId;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getPublisher2() {
+        return publisher2;
+    }
+
+    public void setPublisher2(String publisher2) {
+        this.publisher2 = publisher2;
+    }
+
+    public String getPublisher3() {
+        return publisher3;
+    }
+
+    public void setPublisher3(String publisher3) {
+        this.publisher3 = publisher3;
+    }
+
+    public String getPublisher4() {
+        return publisher4;
+    }
+
+    public void setPublisher4(String publisher4) {
+        this.publisher4 = publisher4;
+    }
+}
